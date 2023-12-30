ff27d1b6324d73dac57a6bc14e21f4772a417e55
ferlucerooo <ferlucero2210@gmail.com>
2023-12-30 02:31:50 -0300

    creacion y estructuracion

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import CharacterList from './components/CharacterList';
+
+function App() {
+
+  return (
+    <div>
+      <h1>rick and morty</h1>
+
+      <CharacterList />
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Character.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Character({character}) {
+  return (
+    <div >
+      <h2>{character.name}</h2>
+      <img src={character.image} alt={character.name} />
+    </div>
+  );
+}
+
+export default Character;
